7918e9fd8d01d0c788d73197eb5fc77727da54ff
m1s <@>
2024-10-24 21:58:02 +0300

    add_lsd edit_scrin_hypr edit_theme_fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,29 @@
+set fish_greeting ""
+
 set -gx TERM xterm-256color
+
+fish_config theme choose "Catppuccin Macchiato"
+
+# alias ll="ls -l --color=auto"
+# alias la="ls -A --color=auto"
+# alias l="ls -CF --color=auto"
+
+alias ls lsd
+alias ll 'ls -hal'
+alias lt 'ls --tree'
+
+set -gx EDITOR nvim
+set -gx KUBE_EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
+# Remove underline a path
+set fish_color_valid_path nornal
+
+starship init fish | source
+
 if not pgrep -u $USER ssh-agent > /dev/null
     eval (ssh-agent -c)
 end
@@ -6,4 +31,3 @@ end
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
